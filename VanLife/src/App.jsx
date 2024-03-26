c32b112a9915e1dfacfc9f5d75c018c36b06a427
Jp Ludin <>
2024-03-26 10:20:39 +0100

    TASK/Add a App component and css for the header

--- VanLife/src/App.jsx
+++ VanLife/src/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import "../css/index.css"
+import "../css/header.css"
 
 export default function App() {
 
   return (
-    <h1>Hello World</h1>
+    <BrowserRouter>
+      <header className='header content-grid'>
+        <nav className="navbar">
+          <Link to="/" className="home-btn">#VANLIFE</Link>
+          <div className='links'>
+            <Link to="/about" className='link'>About</Link>
+
+          </div>        
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+      <footer></footer>     
+   </BrowserRouter>
   )
 }
 
